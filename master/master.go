01735d78968a5b8fce9b1fd5793550e27aa26e44
Christoph Rueff <christoph.rueff@gmail.com>
2019-10-14 18:22:05 +0200

    Implementation working, really messy

--- master/master.go
+++ master/master.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+    "bufio"
+    "bytes"
+    "fmt"
     "log"
     "net"
     "os"
@@ -31,6 +34,10 @@ func main() {
     // redirect stdin to connection
     go util.MustCopy(conn, os.Stdin)
 
+    //
+
+    go receptionist(config)
+
     // sync cycle
     var id int
     for {
@@ -38,6 +45,8 @@ func main() {
         msg := "S," + strconv.Itoa(id)
         sendingTime := time.Now()
 
+        // TODO REMOVE PRINT
+        fmt.Println("sync : " + msg)
         r := strings.NewReader(msg)
         // send message
         util.MustCopy(conn, r)
@@ -45,10 +54,50 @@ func main() {
         // TODO change time unit
         // TODO send bytes
         msg = "F," + strconv.FormatInt(sendingTime.UnixNano(), 10) + ","+ strconv.Itoa(id)
+        // TODO REMOVE println
+        fmt.Println("followup : " + "F," + strconv.FormatInt(sendingTime.UnixNano(), 10) + ","+ strconv.Itoa(id))
         r = strings.NewReader(msg)
         util.MustCopy(conn, r)
 
         // TODO k unit config ! (nano now)
         time.Sleep(time.Duration(config.SyncDelay))
+    }
+}
+
+func receptionist(config util.Config) {
+    conn, err := net.ListenPacket("udp", config.ServerAddr + ":" + config.ServerPort)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    buf := make([]byte, 1024)
+
+    for {
+        n, cliAddr, err := conn.ReadFrom(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        go worker(&conn, cliAddr, buf, n, time.Now().UnixNano())
+    }
+}
+
+func worker(conn *net.PacketConn, cliAddr net.Addr, buf []byte, n int, receiveTime int64) {
+    s := bufio.NewScanner(bytes.NewReader(buf[0:n]))
+    for s.Scan() {
+
+        msg := strings.Split(s.Text(), ",")
+        switch msg[0] {
+        case "R" :
+            res := "Q," + strconv.FormatInt(receiveTime, 10) + "," + msg[1]
+
+            // TODO remove debug
+            fmt.Println("send delay response to : " + cliAddr.String())
+
+            if _, err := (*conn).WriteTo([]byte(res), cliAddr); err != nil {
+                log.Fatal(err)
+            }
+        }
     }
 }
--- slave/slave.go
+++ slave/slave.go
@@ -6,7 +6,6 @@ import (
     "fmt"
     "golang.org/x/net/ipv4"
     "log"
-    "math/rand"
     "net"
     "prr-lab01/common"
     "runtime"
@@ -27,6 +26,7 @@ var step2ready bool
 
 var timeSys int64
 var timeGap int64
+var timeDelay int64
 
 func main() {
     // load address from config file
@@ -98,6 +98,8 @@ func onSync(msg []string) {
 
     syncId = id
     timeSys = time.Now().UnixNano()
+    // TODO REMOVE PRINT
+    fmt.Println("sync from serv : " + strconv.Itoa(id))
 }
 
 func onFollowUp(msg []string) {
@@ -113,18 +115,35 @@ func onFollowUp(msg []string) {
         }
 
         timeGap = timeMaster - timeSys
-        fmt.Println(timeGap)
+        fmt.Println("follow up from serv with gap : " + strconv.FormatInt(timeGap, 10))
 
         if !step2ready {
             step2ready = true
+            // TODO REMOVE PRINT
+            fmt.Println("step 2 ready")
             go delayCorrection()
         }
     }
 }
 
+func onDelayResponse(msg []string, id int) (mTime int64, valid bool){
+    resId, err := strconv.Atoi(msg[2])
+    if err != nil {
+        log.Fatal(err)
+    }
+    var receivedTime int64
+    receivedTime, err = strconv.ParseInt(msg[1], 10, 64)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return receivedTime, id == resId
+}
+
 func delayCorrection() {
-    minDelay := 4 * syncDelay
-    maxDelay := 60 * syncDelay
+    //minDelay := 4 * syncDelay
+    //maxDelay := 10 * syncDelay
+    id := 0
 
     conn, err := net.Dial("udp", srvAddress + ":" + srvPort)
     if err != nil {
@@ -133,11 +152,51 @@ func delayCorrection() {
     defer conn.Close()
 
     for {
-        k := rand.Intn(maxDelay - minDelay) + minDelay
+        // k := rand.Intn(maxDelay - minDelay) + minDelay
+        k := 15 * syncDelay
 
         // wait for next iteration
         time.Sleep(time.Duration(k))
 
         // TODO check setreaddeadline
+
+        // send delay request
+        id++
+        sendTime := time.Now().UnixNano()
+        _ , err := fmt.Fprintf(conn, "R," + strconv.Itoa(id))
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        // TODO REMOVE PRINT
+        fmt.Println("send delay request")
+
+        // wait delay response
+
+        buf := make([]byte, 1024)
+
+        // TODO set goo deadline time (5 sec now)
+        conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+        n, err := conn.Read(buf)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        s := bufio.NewScanner(bytes.NewReader(buf[0:n]))
+        for s.Scan() {
+            // Q -> response delay
+            msg := strings.Split(s.Text(), ",")
+            switch msg[0] {
+            case "Q" :
+                // TODO REMOVE PRINT
+                mTime, valid := onDelayResponse(msg, id)
+                if valid {
+                    timeDelay = (mTime - sendTime) / 2
+                    fmt.Println("received delay resp from serv : delay is : " + strconv.FormatInt(timeDelay, 10))
+                }
+            }
+            fmt.Println("Total decalage : " + strconv.FormatInt(timeDelay + timeGap, 10))
+        }
     }
 }
